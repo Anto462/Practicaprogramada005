adb7bf51905f4b8e11ed0289434628c06281d4a8
dfalm <105889122+dfalm@users.noreply.github.com>
2023-08-17 11:16:25 -0600

    Data Anotations y Formato
    Data Anotations y Formato

--- Practicaprogramada005/Models/Servicio.cs
+++ Practicaprogramada005/Models/Servicio.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using MessagePack;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
 
 namespace Practicaprogramada005.Models
 {
@@ -10,9 +15,17 @@ namespace Practicaprogramada005.Models
             Solicitudes = new HashSet<Solicitude>();
         }
 
+        [Key]
+        [DisplayName("ID Servicio")]
         public int ServiciosId { get; set; }
+
+        [DisplayName("Nombre")]
         public string Nombre { get; set; } = null!;
+
+        [DisplayName("Costo")]
         public int Costo { get; set; }
+
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; } = null!;
 
         public virtual ICollection<Solicitude> Solicitudes { get; set; }
--- Practicaprogramada005/Models/Solicitude.cs
+++ Practicaprogramada005/Models/Solicitude.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using MessagePack;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
 
 namespace Practicaprogramada005.Models
 {
     public partial class Solicitude
     {
+        [Key]
+        [DisplayName("ID Solicitud")]
         public int SolicitudesId { get; set; }
+
+        [DisplayName("ID Servicio")]
         public int ServiciosId { get; set; }
+
+        [DisplayName("ID Usuario")]
         public string Idusuario { get; set; } = null!;
+
+        [DisplayName("Dato 1")]
         public string Datsauto1 { get; set; } = null!;
+
+        [DisplayName("Dato 2")]
         public string Datsauto2 { get; set; } = null!;
+
+        [DisplayName("Dato 3")]
         public string Datsauto3 { get; set; } = null!;
+
+        [DisplayName("Dato 4")]
         public string Datsauto4 { get; set; } = null!;
+
+        [DisplayName("Estado")]
         public string Estadode { get; set; } = null!;
 
         public virtual Servicio Servicios { get; set; } = null!;
